820243061a9b13d1922773eaa0e675bd734448c1
alisha barraw <alisha.barraw@gmail.com>
2024-06-05 13:28:00 -0600

    moved over files

---
+++ errors.js
@@ -0,0 +1,4 @@
+export const zeroOrLess = "Input must be greater than 0";
+
+export const invalidType = "Input is not a valid type";
+
---
+++ index.js
@@ -0,0 +1,15 @@
+import * as errors from './errors.js';
+
+export const calculateSphereVolume = (radius) => {
+    const isInvalidType = typeof radius !== "number" || radius === null || radius === undefined;
+
+    if(isInvalidType) {
+        throw new Error(errors.invalidType);
+    }
+    if(radius <= 0) {
+        throw new Error(errors.zeroOrLess);
+    }
+    return (4 / 3) * Math.PI * Math.pow(radius, 3);
+};
+
+
